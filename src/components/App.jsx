9b51d15a8cb4cfca59c1587c0ae02adcfeaa87a0
Ruslan Samoylenko <ruslan.sem@gmail.com>
2023-01-28 11:24:56 +0200

    Step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,13 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -70,12 +76,25 @@ export class App extends Component {
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
+        <label>
+          Find contacts by name
+          <input
+            type="text"
+            name="filter"
+            value={this.state.filter}
+            onChange={this.handleChange}
+          />
+        </label>
         <ul>
-          {this.state.contacts.map(item => (
-            <li key={item.id}>
-              {item.name}: {item.number}
-            </li>
-          ))}
+          {this.state.contacts
+            .filter(item =>
+              item.name.toLowerCase().includes(this.state.filter.toLowerCase())
+            )
+            .map(item => (
+              <li key={item.id}>
+                {item.name}: {item.number}
+              </li>
+            ))}
         </ul>
       </div>
     );
